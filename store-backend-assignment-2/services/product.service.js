20d87863eb768e0de7500d590105abc3d7b3b10d
yuvalns7 <yuvalns7@gmail.com>
2022-12-10 16:24:53 +0200

    add logs

--- store-backend-assignment-2/services/product.service.js
+++ store-backend-assignment-2/services/product.service.js
@@ -2,8 +2,22 @@ const products = require("../data/products.json")
 const { Product } = require("../models/product.js")
 
 const setUpDb = () => {
-  Product.deleteMany({})
+  Product.deleteMany(
+    {},
+    (res) => {
+      console.log(res)
+    },
+    (e) => {
+      console.log(e)
+    }
+  )
   Product.insertMany(products)
+    .then((res) => {
+      console.log(res)
+    })
+    .catch((e) => {
+      console.log(e)
+    })
 }
 
 module.exports = { setUpDb }
